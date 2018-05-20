2cc95b8205dfc4f5b29c6f3d9c2bb1113147a7fb
Michael Hendricks <michael@ndrix.org>
2018-05-19 19:49:44 -0600

    Simplify decode() with a map lookup
    Instead of writing parsing logic for a bunch of terminal escapes, we
    can just look them up in a table.  This might take a tiny bit longer
    for the executable to start, but the code is much simpler, easier to
    maintain and easier to understand.

--- decode.go
+++ decode.go
@@ -1,182 +1,82 @@
 package getkey // import "github.com/mndrix/getkey"
 import (
-	"errors"
 	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
-	"unicode"
 )
 
-var csiRx = regexp.MustCompile(`^([0-9;]*)([^0-9])$`)
+// map terminal escape sequnces to their descriptive names
+var m map[string]string
+
+func init() {
+	m = make(map[string]string, 256*4)
+
+	// initial control sequences can be generated with Ctrl
+	for i := 0; i <= 26; i++ {
+		k := string([]rune{rune(i)})
+		v := fmt.Sprintf("Ctrl-%c", i+96)
+		//fmt.Fprintf(os.Stderr, "mapping %q to %q\n", k, v)
+		if _, ok := m[k]; !ok {
+			m[k] = v
+		}
+	}
+
+	// printable ASCII is represented as itself
+	for i := 48; i <= 126; i++ {
+		s := fmt.Sprintf("%c", i)
+		if _, ok := m[s]; !ok {
+			m[s] = s
+		}
+	}
+
+	// CSI
+	csi := "\x1b["
+	m[csi+"A"] = "Up"
+	m[csi+"B"] = "Down"
+	m[csi+"C"] = "Right"
+	m[csi+"D"] = "Left"
+	m[csi+"F"] = "End"       // xterm
+	m[csi+"H"] = "Home"      // xterm
+	m[csi+"P"] = "Backspace" // st
+
+	// CSI h
+	m[csi+"4h"] = "Insert"
+
+	// CSI ~
+	m[csi+"1~"] = "Home"
+	m[csi+"2~"] = "Insert"
+	m[csi+"3~"] = "Delete"
+	m[csi+"4~"] = "End"
+	m[csi+"5~"] = "PageUp"
+	m[csi+"6~"] = "PageDown"
+	m[csi+"15~"] = "F5"
+	m[csi+"17~"] = "F6"
+	m[csi+"18~"] = "F7"
+	m[csi+"19~"] = "F8"
+	m[csi+"20~"] = "F9"
+	m[csi+"21~"] = "F10"
+	m[csi+"23~"] = "F11"
+	m[csi+"24~"] = "F12"
+
+	// some function keys
+	z := "\x1bO"
+	m[z+"P"] = "F1"
+	m[z+"Q"] = "F2"
+	m[z+"R"] = "F3"
+	m[z+"S"] = "F4"
+
+	// handle some manually (including overwriting mistakes above)
+	m["\r"] = "Enter"
+	m["\t"] = "Tab"
+	m["\x00"] = "Ctrl-Space"
+	m["\x15"] = "Ctrl-u"
+}
 
 // decode a single sequence of raw bytes from the terminal
 func decode(c []byte) (string, error) {
 	s := string(c)
 	debugf("raw: %v %q", c, s)
-
-	if len(c) == 1 {
-		debugf("single byte")
-		r := rune(c[0])
-		if name := runeName(r); name != "" {
-			return name, nil
-		}
-		if r == 0 {
-			return "Ctrl-" + runeName(' '), nil
-		}
-		if r <= 26 {
-			return "Ctrl-" + runeName(rune(r+96)), nil
-		}
-		debugf("fallthrough")
+	if name, ok := m[s]; ok {
+		debugf("shortcut")
+		return name, nil
 	}
-
-	// parse CSI escapes
-	if strings.HasPrefix(s, "\x1b[") {
-		if name, err := parseCsi(s); err == nil {
-			return name, nil
-		} else {
-			return s, nil
-		}
-	}
-
-	// parse function key escapes
-	if strings.HasPrefix(s, "\x1bO") {
-		if name, err := parseFn(s); err == nil {
-			return name, nil
-		} else {
-			return s, nil
-		}
-	}
-
-	return s, nil
-}
-
-func parseCsi(s string) (string, error) {
-	debugf("CSI")
-	s = strings.TrimPrefix(s, "\x1b[")
-	matches := csiRx.FindStringSubmatch(s)
-	debugf("matches = %v", matches)
-	if matches == nil {
-		return s, nil
-	}
-
-	argStrings := strings.Split(matches[1], ";")
-	args := make([]int, 0, len(argStrings))
-	for _, argString := range argStrings {
-		if argString != "" {
-			arg, err := strconv.Atoi(argString)
-			if err != nil {
-				panic(err)
-			}
-			args = append(args, arg)
-		}
-	}
-	debugf("args = %v", args)
-
-	switch matches[2] {
-	case "~":
-		return csiTilde(args)
-	case "h":
-		return csih(args)
-	case "A":
-		return "Up", nil
-	case "B":
-		return "Down", nil
-	case "C":
-		return "Right", nil
-	case "D":
-		return "Left", nil
-	case "F": // xterm sends this
-		return "End", nil
-	case "H": // xterm sends this
-		return "Home", nil
-	case "P": // st sends this
-		return "Backspace", nil
-	}
-	return s, nil
-}
-
-func parseFn(s string) (string, error) {
-	debugf("Fn")
-	s = strings.TrimPrefix(s, "\x1bO")
-	if s == "" {
-		return s, errors.New("Fn escape missing")
-	}
-
-	switch s {
-	case "P":
-		return "F1", nil
-	case "Q":
-		return "F2", nil
-	case "R":
-		return "F3", nil
-	case "S":
-		return "F4", nil
-	}
-	return s, nil
-}
-
-func csiTilde(args []int) (string, error) {
-	debugf("CSI ~")
-	switch args[0] {
-	case 1:
-		return "Home", nil
-	case 2:
-		return "Insert", nil
-	case 3:
-		return "Delete", nil
-	case 4:
-		return "End", nil
-	case 5:
-		return "PageUp", nil
-	case 6:
-		return "PageDown", nil
-	case 15:
-		return "F5", nil
-	case 17:
-		return "F6", nil
-	case 18:
-		return "F7", nil
-	case 19:
-		return "F8", nil
-	case 20:
-		return "F9", nil
-	case 21:
-		return "F10", nil
-	case 23:
-		return "F11", nil
-	case 24:
-		return "F12", nil
-	}
-	return "", fmt.Errorf("unknown CSI ~ escape: %+v", args)
-}
-
-func csih(args []int) (string, error) {
-	debugf("CSI h")
-	switch args[0] {
-	case 4: // st sends this
-		return "Insert", nil
-	}
-	return "", fmt.Errorf("unknown CSI h escape: %+v", args)
-}
-
-func runeName(r rune) string {
-	switch r {
-	case 8:
-		return "Backspace"
-	case 9:
-		return "Tab"
-	case 13:
-		return "Enter"
-	case 27:
-		return "Escape"
-	case 32:
-		return "Space"
-	case 127:
-		return "Delete"
-	}
-	if r > 31 && unicode.IsPrint(r) {
-		return fmt.Sprintf("%c", r)
-	}
-	return ""
+	return "(unknown)", nil
 }
